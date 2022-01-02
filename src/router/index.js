a02b55f9958dd4b2133c4e993aca8c51c421ca6a
CeZhu <821293938@qq.com>
2022-01-02 17:19:45 +0800

    环境搭建

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: () => import('views/home/Home')
+    }
+  ]
+})
+export default router
---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti placeat sequi, maxime aperiam pariatur et deleniti modi id porro voluptas necessitatibus, recusandae cum error atque obcaecati! Saepe esse optio est!
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
